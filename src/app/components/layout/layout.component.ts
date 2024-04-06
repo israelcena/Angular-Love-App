87e4fb2be6a586077bd6b0c9bc18b08e0cebce8a
israelcena <israelsjm@gmail.com>
2024-04-06 17:56:48 -0300

    include initial app design

---
+++ src/app/components/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+  @Input() title: string = '';
+}
